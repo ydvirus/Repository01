291ba655d42b9e0285231efe2ff765991c9d5f54
Yusuf Dalal <yusufkdalal123@gmail.com>
2020-05-21 15:29:47 +0530

    first commit

---
+++ src/main/java/com/springproject/springproject/IndexController.java
@@ -0,0 +1,14 @@
+package com.springproject.springproject;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class IndexController {
+	@GetMapping("/")
+	public String index()
+	{
+		return "Hello. This is very first exp with heroku !!";
+	}
+
+}
